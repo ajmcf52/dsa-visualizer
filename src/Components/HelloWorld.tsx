809dea353c35ddf518a9792c1e11bb5fec2e6f95
AJ <andrew.mcfarlane@ucalgary.ca>
2021-06-26 15:53:00 -0700

    added gradient background

---
+++ src/Components/HelloWorld.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+class HelloWorld extends React.Component {
+    render() {
+        return <div />;
+    }
+}
+
+export default HelloWorld;
---
+++ src/Components/VisualizationSpace/VisualizationSpace.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './VisualizationSpace.css'
+
+class VisualizationSpace extends React.Component {
+    render() {
+        return <div className='visualizationSpace'/>;
+    }
+}
+
+export default VisualizationSpace;
