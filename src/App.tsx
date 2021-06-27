427819f4e4a547fca16670916117a4fc6c5d5cd4
AJ <andrew.mcfarlane@ucalgary.ca>
2021-06-26 18:45:13 -0700

    graph node animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GraphNode from './Components/Graph/GraphNode';
 import VisualizationSpace from './Components/VisualizationSpace'
 
 function App() {
   return (
     <div className="App">
       <VisualizationSpace/>
+
+      <GraphNode label='A'/>
     </div>
   );
 }
---
+++ src/Components/Graph/GraphNode/GraphNode.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './GraphNode.css'
+
+interface GraphNodeProps {
+    label: string
+}
+
+interface GraphNodeState {
+    visited: boolean
+}
+
+class GraphNode extends React.Component<GraphNodeProps, GraphNodeState> {
+    constructor(props: GraphNodeProps) {
+        super(props)
+        this.state = {
+            visited: false
+        }
+    }
+    
+    render() {
+        return <span className='graphNode'><p className='nodeText'>{this.props.label}</p></span>
+    }
+}
+
+
+export default GraphNode;
