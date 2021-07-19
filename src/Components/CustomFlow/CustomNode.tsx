c5292a21e0f0bfdaf7a40f359f53d749d3a55e00
AJ <andrew.mcfarlane@ucalgary.ca>
2021-07-19 09:13:10 -0700

    reworked handle props

--- src/Components/CustomFlow/CustomNode.tsx
+++ src/Components/CustomFlow/CustomNode.tsx
@@ -12,10 +12,6 @@ const nodeStyle = {
     background: NodeBackgroundColor
 }
 
-const handleStyle = {
-    background: NodeHandleColor
-}
-
 class CustomNode extends React.Component<CustomNodeProps> {
     componentDidUpdate() {
         var node: HTMLDivElement | null = document.querySelector('.customNode div')
@@ -29,23 +25,37 @@ class CustomNode extends React.Component<CustomNodeProps> {
 
         /**
          * TODO 
-         * - assign correct IDs to each of the handles
-         * - verify that we have all the props required for this class
-         * - verify structural integrity of the component
+         * - assign correct IDs to each of the handles ... DONE.
+         * - verify that we have all the props required for this class ... DONE.
+         * - verify structural integrity of the component ... DONE.
          * 
          * and later... test!
          */
 
+
+        /**
+         * NOTE may need to mess around with HOW we assign background color with respect to node
+         * visitation...
+         */
+
         return <div className='customNode' style={nodeStyle}>
             <p className='nodeText'>{label}</p>
-            <Handle type='target' position={Position.Left} style={handleStyle}/>
-            <Handle type='target' position={Position.Right} style={handleStyle}/>
-            <Handle type='target' position={Position.Bottom} style={handleStyle}/>
-            <Handle type='target' position={Position.Top} style={handleStyle}/>
-            <Handle type='source' position={Position.Left} style={handleStyle}/>
-            <Handle type='source' position={Position.Right} style={handleStyle}/>
-            <Handle type='source' position={Position.Bottom} style={handleStyle}/>
-            <Handle type='source' position={Position.Top} style={handleStyle}/>
+            <Handle type='target' position={Position.Left} style={{backgroundColor:NodeHandleColor,
+            top:'50%'}} id='lt'/>
+            <Handle type='target' position={Position.Right} style={{backgroundColor:NodeHandleColor,
+            top:'50%'}} id='rt'/>
+            <Handle type='target' position={Position.Bottom} style={{backgroundColor:NodeHandleColor,
+            left:'50%'}} id='bt'/>
+            <Handle type='target' position={Position.Top} style={{backgroundColor:NodeHandleColor,
+            left:'50%'}} id='tt'/>
+            <Handle type='source' position={Position.Left} style={{backgroundColor:NodeHandleColor,
+            top:'50%'}} id='ls'/>
+            <Handle type='source' position={Position.Right} style={{backgroundColor:NodeHandleColor,
+            top:'50%'}} id='rs'/>
+            <Handle type='source' position={Position.Bottom} style={{backgroundColor:NodeHandleColor,
+            left:'50%'}} id='bs'/>
+            <Handle type='source' position={Position.Top} style={{backgroundColor:NodeHandleColor,
+            left:'50%'}} id='ts'/>
         </div>
     }
 }
