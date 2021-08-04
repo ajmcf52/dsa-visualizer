85416d7fb4c2047d6d37aa1cbd449a9be8e39706
AJ <andrew.mcfarlane52@gmail.com>
2021-08-03 20:29:04 -0700

    tidied up the select component for 'start node'; things are starting to come together.

--- src/Components/ControlPanel/SearchControl/SearchControl.tsx
+++ src/Components/ControlPanel/SearchControl/SearchControl.tsx
@@ -2,8 +2,46 @@ import './SearchControl.css'
 import React from 'react'
 import { connect } from 'react-redux'
 import { ControlSettingsEventCreator } from '../../../Actions/ControlSettingsEvent'
-import { FormControl, InputLabel, Select, MenuItem, Box } from '@material-ui/core'
+import { FormControl, InputLabel, Select, MenuItem, InputBase, Input } from '@material-ui/core'
+import { createStyles, makeStyles, Theme, withStyles } from '@material-ui/core/styles'
 import { RootState } from '../../../store'
+import { relative } from 'path/posix'
+
+const BootstrapInput = withStyles((theme: Theme) => 
+    createStyles({
+        root: {
+            'label + &': {
+                marginTop: theme.spacing(3),
+            }
+        },
+        input: {
+            borderRadius: 4,
+            fontSize: 16,
+            border: '1px solid #ced4da',
+            position: 'relative',
+            backgroundColor: theme.palette.background.paper,
+            padding: '10px 26px 10px 12px',
+            transition: theme.transitions.create(['border-color', 'box-shadow']),
+            fontFamily: ['Courier New'].join(','),
+            '&:focus': {
+                borderColor: '#80bdff',
+                boxShadow: '0 0 0 0.2rem rgba(0,123,255,0.25)'
+            }
+        }
+    }))(InputBase)
+
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth: 60
+        },
+        selection: {
+            padding: '10px 26px 10px 12px'
+        }
+    })
+
+)
 
 interface SearchControlProps {
     startNode: string,
@@ -14,16 +52,17 @@ interface SearchControlProps {
 
 const SearchControl = (props: SearchControlProps) => {
     const { startNode, goalNode, chooseStartNode, vertexList } = props
-    
-    return (<Box className='selectStartNodeBox'>
-        <FormControl>
-            <InputLabel id='selectStartNodeLabel'>Start Node</InputLabel>
+    const classes = useStyles()
+    return (
+        <FormControl className={classes.formControl}>
+            <InputLabel id='selectStartNodeLabel'>Start</InputLabel>
             <Select
                 labelId='selectStartNodeLabel'
                 id='selectStartNode'
                 value={startNode}
-                label='Start Node'
+                label='Start'
                 onChange={(e) => chooseStartNode(e.currentTarget.value as string)}
+                input={<BootstrapInput />}
             >
             {vertexList.map((value) => {
                 return<MenuItem value={value}>{value}</MenuItem>
@@ -31,7 +70,7 @@ const SearchControl = (props: SearchControlProps) => {
 
             </Select>
         </FormControl>
-    </Box>)
+    )
 }
 
 const mapStateToProps = (state: RootState, props: SearchControlProps) => ({
